28206cc8a5eb43205a487f22ba14db11cacf8eda
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-09-05 23:30:45 +0100

    Header component

---
+++ src/Header/Header.js
@@ -0,0 +1,34 @@
+import classes from './Header.module.css';
+
+const Header = (props) => {
+    return ( 
+        <header class={classes.header}>
+            <div className={classes.box}>
+                <div className={classes['box-1-of-2']}>
+                    <div className={classes['header-description']}>
+                        <h1 className={classes['heading-main']}>Hello, I'm <br /> Emmanuel <span>Elugbaju</span></h1>
+                        <h2 className={classes['heading-sub']}>Front-End <span>Developer</span></h2>
+                        <p className={classes['long-copy']}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias inventore rem fuga aut, ratione nihil corrupti accusantium eos dolore ad maxime accusamus assumenda cupiditate unde numquam blanditiis nobis minus atque?</p>
+                        <div className={classes['header-social-links']}>
+                            <div className={classes['hsl-box-1-of-2']}>
+                                <a href='https://www.linkedin.com/in/emmanuel-elugbaju'>
+                                    <i class="fa-brands fa-linkedin-in"></i>LinkedIn
+                                </a>
+                            </div>
+                            <div className={classes['hsl-box-1-of-2']}>
+                                <a href='https://github.com/elughsmanuel'>
+                                    <i class="fa-brands fa-github"></i>GitHub
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={classes['box-1-of-2']}>
+                    B
+                </div>
+            </div>
+        </header>
+     );
+}
+ 
+export default Header;
