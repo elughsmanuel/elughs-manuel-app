b2a32594b2db73b40295139d1891e818c26d096e
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-09-13 16:55:01 +0100

    Twitter link added

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -7,6 +7,7 @@ const Contact = (props) => {
                 <div className={classes['contact-box-1-of-2']}>
                     <a href='https://www.linkedin.com/in/emmanuel-elugbaju'><i className="fa-brands fa-linkedin"></i>LinkedIn</a>
                     <a href='https://github.com/elughsmanuel'><i className="fa-brands fa-github"></i>GitHub</a>
+                    <a href='https://twitter.com/elughsm'><i className="fa-brands fa-twitter"></i>Twitter</a>
                 </div>  
                 <div className={classes['contact-box-1-of-2']}>
                     <p>Lagos, Nigeria</p>
